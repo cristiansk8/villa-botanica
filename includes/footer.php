2b9a8bf1f01067a4d6d534515bd80d70d9381369
Camilo <kmilo.vanegas@gmail.com>
2020-02-28 05:10:14 -0500

    CSS/footer display and middle blocks

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,14 @@
-<footer class="container site-footer">
-  <hr/>
-	<div class="row">
-    <?php dynamic_sidebar('footer-widget-area'); ?>
-  </div>
-  <hr/>
-  <div class="row">
-    <div class="col-lg-12 site-sub-footer">
-      <p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></p>
+<footer class="site-footer">
+  <div class="site-sub-footer__wrapper container">
+    <hr/>
+    <div class="row">
+      <?php dynamic_sidebar('footer-widget-area'); ?>
+    </div>
+    <hr/>
+    <div class="row">
+      <div class="col-lg-12 site-sub-footer">
+        <p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></p>
+      </div>
     </div>
   </div>
 </footer>
