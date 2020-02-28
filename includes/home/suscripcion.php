68ef44bbcf73dbbdd4b4274996451f541e8b8195
unknown <cdpsk8@gmail.com>
2020-02-28 16:16:54 -0500

    ajuste contenido

--- includes/home/suscripcion.php
+++ includes/home/suscripcion.php
@@ -4,7 +4,9 @@
       <h2>GET IN TOUCH</h2>
     </div>
     <div class="formulario--descripcion">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pretium rutrum consequat. Praesent eget purus sapien. Curabitur elementum pellentesque nibh, et consequat lorem laoreet ac</p>
+      <p>We would love to hear from you. Our attentive staff are ready to answer any
+questions to make your dream come true and show you why Villa Botanica is the
+best choice for your wedding or event. </p>
     </div>
     <div class="formulario--form">
       <?php echo do_shortcode('[contact-form-7 id="71" title="Contact form 1"]'); ?>
